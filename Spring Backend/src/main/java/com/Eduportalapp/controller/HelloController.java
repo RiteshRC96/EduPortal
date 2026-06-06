556ec3a7090a71f6cfea49bdc77106edfcf37b25
Shekhar Patil <shekhar.patil@smartrecontech.com>
2026-06-06 19:36:06 +0530

    change in controller

--- Spring Backend/src/main/java/com/Eduportalapp/controller/HelloController.java
+++ Spring Backend/src/main/java/com/Eduportalapp/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/theme")
     public String getTheme() {
-        return "dark";
+        return "home controller";
     }
 }
