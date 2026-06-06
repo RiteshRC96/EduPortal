7868e4130eb7b4f1d7d5b374aa9fa79b30940169
RiteshChougule <RiteshChougule96@gmail.com>
2026-06-06 19:10:41 +0530

    Initial Commit

---
+++ src/main/java/com/Eduportalapp/DemApplication.java
@@ -0,0 +1,12 @@
+package com.Eduportalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemApplication.class, args);
+    }
+}
---
+++ src/main/java/com/Eduportalapp/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.Eduportalapp.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class HelloController {
+    @GetMapping("/api/theme")
+    public String getTheme() {
+        return "dark";
+    }
+}
